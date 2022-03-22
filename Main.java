77d50ef065dd07ee68948971745f6f6d5e5a885e
Bartek5756 <101180607+Bartek5756@users.noreply.github.com>
2022-03-22 10:03:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a=10;
+    char b = 'z';
+    double c = -12;
+    byte d =6;
+    
+    System.out.println("int=" +a);
+    System.out.println("char=" +b);
+    System.out.println("double=" +c);
+    System.out.println("byte=" +d);
+  }
+}
